17a9be46a8e1fe51a45a40c5387cdd009a0916d5
DArkbluster03 <darkbluster03@gmail.com>
2025-02-10 02:59:42 +0600

    tailewind css added

--- backend/src/main.js
+++ backend/src/main.js
@@ -2,10 +2,14 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import './index.css'
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
 
-app.use(router)
+app
+.use(store)
+.use(router)
 
 app.mount('#app')
